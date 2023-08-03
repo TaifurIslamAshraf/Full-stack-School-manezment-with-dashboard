a1dea8dcb3d46d16e20e103905248cfbb019bd04
MD. Taifur <Taifurislamashraf.bd@gmail.com>
2023-08-03 20:03:26 +0600

    footer done

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,14 +1,34 @@
+import FooterLogo from "../../assets/images/education.png";
 import "./footer.css";
 
 const Footer = () => {
   return (
     <div className="footer-contant">
       <div className="main-footer">
-        <div className="footer-desc">Footer Decsriptions</div>
-        <div className="footer-link1">Footer Link 1</div>
-        <div className="footer-link2">Footer Link 2</div>
+        <div className="footer-desc">
+          <img src={FooterLogo} alt="footer logo" />
+          <p>
+            Let the path of education light always shine, so we are progressing
+            on the bright path of education. We are moving in a completely
+            reliable and trustworthy way to provide a complete all education
+            preparation.
+          </p>
+        </div>
+        <div className="footer-link1">
+          <a href="/">Home</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+        </div>
+        <div className="footer-link2">
+          <a href="#">Articale</a>
+          <a href="#">Notice</a>
+          <a href="#">Result</a>
+        </div>
       </div>
-      <div className="copyright">CopyRight Section</div>
+      <div className="copyright">
+        <small>&copy; copyright by MD. Taifur</small>
+        <small>Develop by MD. Taifur</small>
+      </div>
     </div>
   );
 };
